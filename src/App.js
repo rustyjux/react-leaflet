5c431095bfe2bda67c118131c2a12b52d8f7964f
Russell Vinegar <russell.vinegar@gov.bc.ca>
2023-11-26 12:36:50 -0800

    add test data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import './App.css';
+import treeData from "./test-data/tree-data.json"
 
 function App() {
+
+    // Get only actual tree points, not comments
+  const treeItems = treeData.features.filter(tree => tree.geometry);
+
   return (
-    <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer center={[49.0852959397798, -117.793012400236]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {treeItems.map(tree => {
+        return (
+          <Marker 
+          // className = {tree.properties.Type}
+          key = {tree.id}
+          position={[ tree.geometry.coordinates[1], tree.geometry.coordinates[0] ]}>
+            <Popup
+              position={[ tree.geometry.coordinates[1], tree.geometry.coordinates[0] ]}>
+              <div>
+                <h2>{"Tree type: " + tree.properties.Type}</h2>
+              </div>
+            </Popup>
+          </Marker>
+        );
+        })}      
+    </MapContainer>
   );
 }
 
